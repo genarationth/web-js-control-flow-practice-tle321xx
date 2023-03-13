5d558ff3f60e94e492a2974ec868a7a3e2d86cc9
tle321xx <125847689+tle321xx@users.noreply.github.com>
2023-03-13 06:38:54 +0000

    addDayNumber

---
+++ daynumber.js
@@ -0,0 +1,13 @@
+let dayNUmber = 0;
+switch (dayNUmber) {
+    case 0: dayNUmber = "Sunday"; break;
+    case 1: dayNUmber = "Monday"; break;
+    case 2: dayNUmber = "Tuesday"; break;
+    case 3: dayNUmber = "Wednesday"; break;
+    case 4: dayNUmber = "Thursday"; break;
+    case 5: dayNUmber = "Friday"; break;
+    case 6: dayNUmber = "Saturday"; break;
+    default:
+    console.log("Invalid day number");
+}
+console.log(dayNUmber);
